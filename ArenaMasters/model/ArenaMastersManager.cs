65d1150f523078893665548d520350cd12df9aa0
JavierMarGon <javier.martin.gonzalez@alumnojoyfe.iepgroup.es>
2024-01-26 18:52:08 +0100

    0.1.5

--- ArenaMasters/model/ArenaMastersManager.cs
+++ ArenaMasters/model/ArenaMastersManager.cs
@@ -15,168 +15,167 @@ using System.Windows.Input;
 namespace ArenaMasters.model
 {
 
-    class ArenaMastersManager : INotifyPropertyChanged
-    {
-    public event PropertyChangedEventHandler PropertyChanged;
+    class ArenaMastersManager : INotifyPropertyChanged{
+
+        public event PropertyChangedEventHandler PropertyChanged;
     
 
-    private DataAccess _ad = new DataAccess();
+        private DataAccess _ad = new DataAccess();
 
 
-    //Campos privados
+        //Campos privados
 
-    ObservableCollection<Units> _unitList;
-    ObservableCollection<Partida> _gameList;
-    private int _id_User;
-    private string _userName;
-    private MainWindow _mainWindow;
+        ObservableCollection<Units> _unitList;
+        ObservableCollection<Partida> _gameList;
+        private int _id_User;
+        private string _userName;
+        private MainWindow _mainWindow;
         //Propiedades (campos publicos)
-    public MainWindow MainWindow
+        public MainWindow MainWindow
         {
             get { return _mainWindow;}
             set { _mainWindow = value; }
         }
-    public int id_User
+        public int id_User
         {
             get { return _id_User; }
             set { _id_User = value; }
         }  
-    public string userName
+        public string userName
         {
             get { return _userName; }
             set { _userName = value; }
         }
-    public ObservableCollection<Units> UnitList
-    {
-        get { return _unitList; }
-    }
+        public ObservableCollection<Units> UnitList
+        {
+            get { return _unitList; }
+        }
 
-    public ObservableCollection<Partida> GameList
-    {
-        get { return _gameList; }
-    }
+        public ObservableCollection<Partida> GameList
+        {
+            get { return _gameList; }
+        }
     
         //Constructor(es)
         public ArenaMastersManager()
-    {
-        _unitList = new ObservableCollection<Units>();
-        _gameList = new ObservableCollection<Partida>();
-        
+        {
+            _unitList = new ObservableCollection<Units>();
+            _gameList = new ObservableCollection<Partida>();
         }
         //Metodos (de Negocio)
         
         public int Login(string usuario, string pass)
-    {
+        {
             //Comprobaciones previas
-        int id_user = _ad.PA_Login(usuario, pass);
-        if (id_user > 0)
-        {
-            MessageBox.Show("Bienvenid@ ", usuario);
-            return id_user;
-        }
-        else
-        {
-            MessageBox.Show("Error de Login");
-            return -1;
-        }
-    }
-    public int Register(string nombre,  string pass)
-    {
-        //Comprobaciones previas
-
-        if (_ad.PA_Register(nombre, pass) == 1)
-        {
-            MessageBox.Show("Bienvenid@");
-            return 1;
-        }
-        else
-        {
-            MessageBox.Show("Error de Login");
-            return -1;
-        }
-    }
-
-    public int GetUser(string name, string psw)
-    {
-        int id_user = _ad.PA_FindUser(name, psw);
-        if (id_user > 0)
-        {
-            return id_user;
-        }
-        else
-        {
-            MessageBox.Show("Error GetUser");
-            return -1;
-        }
-    }
-
-    public int ContinueGame(int id_user)
-    {
-        int id_game = _ad.PA_ContinueGame(id_user);
-        if (id_game > 0)
-        {
-            return id_game;
-        }
-        else
-        {
-            MessageBox.Show("Error ContinueGame");
-            return -1;
-        }
-    }
-
-    public Game GetGame(int id_game, string name, int id_user)
-    {
-        string jsonResult = _ad.PA_GetGameData(id_game);   //Obtengo los datos de la partida
-        var gameData = JsonConvert.DeserializeAnonymousType(jsonResult, new
-        {
-            Res = 0,
-            Money = 0,
-            Round = 0,
-            Refresh = 0
-        });
-
-        if(gameData != null)
+            int id_user = _ad.PA_Login(usuario, pass);
+            if (id_user > 0)
             {
-                return new Game(
-                                id_game,
-                                id_user,
-                                name,                                  
-                                gameData.Round,
-                                gameData.Refresh,
-                                gameData.Money 
-                                );
+                MessageBox.Show("Bienvenid@ ", usuario);
+                return id_user;
             }
+            else
+            {
+                MessageBox.Show("Error de Login");
+                return -1;
+            }
+        }
+        public int Register(string nombre,  string pass)
+        {
+            //Comprobaciones previas
+
+            if (_ad.PA_Register(nombre, pass) == 1)
+            {
+                MessageBox.Show("Bienvenid@");
+                return 1;
+            }
+            else
+            {
+                MessageBox.Show("Error de Login");
+                return -1;
+            }
+        }
+
+        public int GetUser(string name, string psw)
+        {
+            int id_user = _ad.PA_FindUser(name, psw);
+            if (id_user > 0)
+            {
+                return id_user;
+            }
+            else
+            {
+                MessageBox.Show("Error GetUser");
+                return -1;
+            }
+        }
+
+        public int ContinueGame(int id_user)
+        {
+            int id_game = _ad.PA_ContinueGame(id_user);
+            if (id_game > 0)
+            {
+                return id_game;
+            }
+            else
+            {
+                MessageBox.Show("Error ContinueGame");
+                return -1;
+            }
+        }
+
+        public Game GetGame(int id_game, string name, int id_user)
+        {
+            string jsonResult = _ad.PA_GetGameData(id_game);   //Obtengo los datos de la partida
+            var gameData = JsonConvert.DeserializeAnonymousType(jsonResult, new
+            {
+                Res = 0,
+                Money = 0,
+                Round = 0,
+                Refresh = 0
+            });
+
+            if(gameData != null)
+                {
+                    return new Game(
+                                    id_game,
+                                    id_user,
+                                    name,                                  
+                                    gameData.Round,
+                                    gameData.Refresh,
+                                    gameData.Money 
+                                    );
+                }
         
-        return null;
-    }
+            return null;
+        }
        
 
         public void GetAllGames(int id_user)
-    {
+        {
             _gameList.Clear();
 
             DataSet dataGames = new DataSet();
-        dataGames = _ad.PA_GetAllGames(id_user);
+            dataGames = _ad.PA_GetAllGames(id_user);
         
-        foreach (DataRow dr in dataGames.Tables[0].Rows)
-        {
-            Partida p;
-            p = new Partida(id_User,
-                            userName,
-                            int.Parse(dr.ItemArray[0].ToString()),
-                            int.Parse(dr.ItemArray[1].ToString()),
-                            int.Parse(dr.ItemArray[2].ToString()),
-                            DateTime.Parse(dr.ItemArray[3].ToString()),
-                            MainWindow,
-                            this);
-            _gameList.Add(p);
+            foreach (DataRow dr in dataGames.Tables[0].Rows)
+            {
+                Partida p;
+                p = new Partida(id_User,
+                                userName,
+                                int.Parse(dr.ItemArray[0].ToString()),
+                                int.Parse(dr.ItemArray[1].ToString()),
+                                int.Parse(dr.ItemArray[2].ToString()),
+                                DateTime.Parse(dr.ItemArray[3].ToString()),
+                                MainWindow,
+                                this);
+                _gameList.Add(p);
+            }
+            OnPropertyChanged("GameList");
         }
-        OnPropertyChanged("GameList");
-    }
 
-    protected void OnPropertyChanged([CallerMemberName] string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
-}
 }
--- ArenaMasters/obj/Debug/net6.0-windows/ArenaMasters.AssemblyInfo.cs
+++ ArenaMasters/obj/Debug/net6.0-windows/ArenaMasters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArenaMasters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6c91ffb9f51a0b4e979996730d3ec9d076f9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArenaMasters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArenaMasters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
